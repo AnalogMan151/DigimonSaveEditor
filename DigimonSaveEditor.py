7946dbf8cbce571a41b1fe5f2457ab701fb63aa0
AnalogMan <james.p.wolfe@outlook.com>
2020-01-28 23:09:45 -0500

    Fix issue #1
    Fix #1 
    Added duplicate item detection and prevention
    Replace to_byte with struct packs
    Perform edits on a temporary inventory before writing back to file

--- DigimonSaveEditor.py
+++ DigimonSaveEditor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Author: AnalogMan
-# Modified Date: 2019-11-03
+# Modified Date: 2020-01-28
 # Purpose: Applies various edits to a Digimon Story Cyber Sleuth Complete Edition for Nintento Switch save file
 # Usage: DigmonSaveEditor.py
 
@@ -11,6 +11,7 @@ if version_info <= (3,2,0):
 
 from os import remove
 from shutil import copy2
+import struct
 
 CS_Inv_Addr = 0x3EB30
 HM_Inv_Addr = 0xA02D0
@@ -33,100 +34,120 @@ HM_Points_Addr = 0xAD15C
 
 CYBER_SLEUTH = 1; HACKERS_MEMORY = 2
 
-def addToInventory(filepath, inv_addr, item_list, item_type, item_qty):
+# Struct(occupied, item slot, item type, item number, item number, quantity)
+item_data = struct.Struct('< I I I I I I')
+
+# All current valid items except key items
+consumables = list(range(1, 34)) + list(range(50, 59)) + list(range(60, 68)) + list(range(70, 80)) + list(range(90, 92)) \
+                + list(range(100, 107)) + list(range(110, 128)) + list(range(200, 231))
+equipment = list(range(301, 404))
+farm = list(range(501,510)) + list(range(520, 540))
+medals = list(range(1001,1701))
+accessories = [10101, 12401, 10201, 12501, 10301, 10302, 12601, 
+            10401, 10501, 10502, 12701, 12702, 12801, 
+            12901, 13001, 10701, 10801, 10901, 10902, 
+            13101, 13102, 13201, 11001, 11101, 13301, 
+            13401, 11201, 11301, 13501, 11401, 11402, 
+            11403, 13601, 11501, 11601, 13701, 13801, 
+            13901, 11701, 11702, 11703, 14001, 14101, 
+            11801, 11802, 14201, 11901, 12001, 12002, 
+            12003, 14301, 12101, 12102, 12103, 14401, 
+            14402, 14501, 12201, 12301, 12302, 12303]
+
+# Reads inventory from file and returns full inventory and currently obtained item IDs
+def buildInventory(filepath, inv_addr):
+    inventory = []
+    values = []
+    existing_items = []
     try:
-        # Open file
+        # Open file and read full inventory into list
+        with open(filepath, 'rb') as f:
+            f.seek(inv_addr)
+            for _ in range(2000):
+                values = list(item_data.unpack(f.read(24)))
+                inventory.append(values)
+    except:
+        raise
+
+    # Iterate through inventory and adding item ID to list
+    i = 0
+    while i < 2000:
+        if inventory[i][0] & 1 == 1:
+            existing_items.append(inventory[i][3])
+        i += 1
+    return inventory, existing_items
+
+# Reads inventory, seeks to first empty slot, adds passed item then writes new inventory back
+def addToInventory(filepath, inv_addr, item_list, item_type, item_qty, duplicate=False):
+    inventory, _ = buildInventory(filepath, inv_addr)
+    skip_items = []
+    if duplicate == False:
+        for item in inventory:
+            if item[0] & 1 == 1:
+                if item[3] in item_list:
+                    item[5] = (item[5] & 0xFFFFFF00) + item_qty
+                    skip_items.append(item[3]) 
+    i = 0
+    while i < 2000:
+        if inventory[i][0] & 1 == 0:
+            for item in item_list:
+                if item not in skip_items:
+                    inventory[i] = [0x3F800001, i, item_type, item, item, item_qty]
+                    i +=1
+            break
+        i += 1
+    
+    try:
         with open(filepath, 'rb+') as f:
             f.seek(inv_addr)
-            item_slot = 0
-            # Make sure to not go beyond inventory end
-            while item_slot < 2000:
-                occupied = f.read(4)
-                # Check if slot is occupied by an item already. If so, move on to next slot
-                if int.from_bytes(occupied, 'little') & 1 == 1:
-                    f.seek(20,1)
-                    item_slot += 1
-                    continue
-                f.seek(-4,1)
-                break
-            index = 0
-            while index < len(item_list) and item_slot < 2000:
-                occupied = f.read(4)
-                # Recheck for empty slot in case empty slots aren't continuous (shouldn't happen)
-                if int.from_bytes(occupied, 'little') & 1 == 1:
-                    f.seek(20,1)
-                    item_slot += 1
-                    continue
-                f.seek(-4,1)
-                # Write item and move to next empty slot
-                f.write(0x3F800001.to_bytes(4, 'little'))
-                f.seek(4,1)
-                f.write(int(item_type).to_bytes(4, 'little'))
-                f.write(item_list[index].to_bytes(4, 'little'))
-                f.write(item_list[index].to_bytes(4, 'little'))
-                f.write(int(item_qty).to_bytes(4, 'little'))
-                index += 1
-                item_slot += 1
+            for item in inventory:
+                f.write(item_data.pack(*item))
         return 0
     except:
         return 1
 
-def overwriteInventory(filepath, inv_addr, item_list, item_type, item_qty):
-    try:
-        # Open file
-        with open(filepath, 'rb+') as f:
-            f.seek(inv_addr)
-            item_slot = 0
-            index = 0
-            while index < len(item_list) and item_slot < 2000:
-                occupied = f.read(4)
-                if int.from_bytes(occupied, 'little') & 1 == 1:
-                    f.seek(4,1)
-                    current_item_type = f.read(4)
-                    # If a key item, skip to next item slot
-                    if int.from_bytes(current_item_type, 'little') == KEY_ITEM:
-                        f.seek(12,1)
-                        item_slot += 1
-                        continue
-                    else:
-                        f.seek(-8,1)
-                f.seek(-4,1)
-                # Write item and move to next slot
-                f.write(0x3F800001.to_bytes(4, 'little'))
-                f.seek(4,1)
-                f.write(int(item_type).to_bytes(4, 'little'))
-                f.write(item_list[index].to_bytes(4, 'little'))
-                f.write(item_list[index].to_bytes(4, 'little'))
-                f.write(int(item_qty).to_bytes(4, 'little'))
-                index += 1
-                item_slot += 1
-            return f.tell()
-    except:
-        raise
+# Increases all item quantities except Key Items and Medals to 95, 
+# then finds empty space and adds remaining missing items
+def overwriteInventory(inventory, existing_items):       
+    i = 0
+    while i < 2000:
+        if inventory[i][0] & 1 == 1:
+            if inventory[i][2] != KEY_ITEM and inventory[i][2] != MEDAL_ITEM:
+                inventory[i][5] = (inventory[i][5] & 0xFFFFFF00) + 95
+
+        if inventory[i][0] & 1 == 0:
+            for item in consumables:
+                if item not in existing_items:
+                    inventory[i] = [0x3F800001, i, USABLE_ITEM, item, item, 95]
+                    i += 1
+            for item in equipment:
+                if item not in existing_items:
+                    inventory[i] = [0x3F800001, i, EQUIP_ITEM, item, item, 95]
+                    i += 1
+            for item in farm:
+                if item not in existing_items:
+                    inventory[i] = [0x3F800001, i, FARM_ITEM, item, item, 95]
+                    i += 1
+            for item in medals:
+                if item not in existing_items:
+                    inventory[i] = [0x3F800001, i, MEDAL_ITEM, item, item, 1]
+                    i += 1
+            for item in accessories:
+                if item not in existing_items:
+                    inventory[i] = [0x3F800001, i, ACCESSORY_ITEM, item, item, 95]
+                    i += 1
+            break
+        i += 1
+    return inventory
 
 def allItems(filepath, inv_addr):
-    consumables = list(range(1, 34)) + list(range(50, 59)) + list(range(60, 68)) + list(range(70, 80)) + list(range(90, 92)) \
-                    + list(range(100, 107)) + list(range(110, 128)) + list(range(200, 231))
-    equipment = list(range(301, 404))
-    farm = list(range(501,510)) + list(range(520, 540))
-    medals = list(range(1001,1701))
-    accessories = [10101, 12401, 10201, 12501, 10301, 10302, 12601, 
-                10401, 10501, 10502, 12701, 12702, 12801, 
-                12901, 13001, 10701, 10801, 10901, 10902, 
-                13101, 13102, 13201, 11001, 11101, 13301, 
-                13401, 11201, 11301, 13501, 11401, 11402, 
-                11403, 13601, 11501, 11601, 13701, 13801, 
-                13901, 11701, 11702, 11703, 14001, 14101, 
-                11801, 11802, 14201, 11901, 12001, 12002, 
-                12003, 14301, 12101, 12102, 12103, 14401, 
-                14402, 14501, 12201, 12301, 12302, 12303]
-    
+    inventory, existing_items = buildInventory(filepath, inv_addr)
+    data = overwriteInventory(inventory, existing_items)
     try:
-        offset = overwriteInventory(filepath, inv_addr, consumables, USABLE_ITEM, 95)
-        offset = overwriteInventory(filepath, offset, equipment, EQUIP_ITEM, 95)
-        offset = overwriteInventory(filepath, offset, farm, FARM_ITEM, 95)
-        offset = overwriteInventory(filepath, offset, medals, MEDAL_ITEM, 1)
-        overwriteInventory(filepath, offset, accessories, ACCESSORY_ITEM, 95)
+        with open(filepath, 'rb+') as f:
+            f.seek(inv_addr)
+            for item in data:
+                f.write(item_data.pack(*item))
         return 0
     except:
         return 1
@@ -135,12 +156,25 @@ def write32(filepath, addr, value):
     try:
         with open(filepath, 'rb+') as f:
             f.seek(addr)
-            f.write(int(value).to_bytes(4, 'little'))
+            f.write(struct.pack('< I', value))
+        return 0
+    except:
+        return 1
+
+def write16(filepath, addr, value):
+    try:
+        with open(filepath, 'rb+') as f:
+            f.seek(addr)
+            f.write(struct.pack('< H', value))
         return 0
     except:
         return 1
 
 def main():
+    global medals
+    global equipment
+    global accessories
+
     print('\n\n==== Digimon Story Cyber Sleuth: Complete Edition Save Editor ====\n')
     ret = 0
     if len(argv) > 1:
@@ -162,9 +196,7 @@ def main():
         return 1
 
     try:
-        cheat = int(input('\n\nChoose a modification\n'
-        'Note: With inventory cheats it\'s best to sell off existing items to\n'
-        '      prevent duplicate entries\n\n'
+        cheat = int(input('\n\nChoose a modification\n\n'
         '1)  Add all medals to inventory                        (700 inventory slots)\n'
         '2)  Add 5 stacks of Popular Guy\'s Guide to inventory     (5 inventory slots)\n'
         '3)  Add 99x of all stat increasing foods to inventory    (7 inventory slots)\n'
@@ -192,12 +224,11 @@ def main():
     if game == CYBER_SLEUTH or game == (CYBER_SLEUTH + HACKERS_MEMORY):
         print('Executing cheat for Cyber Sleuth...')
         if cheat == 1:
-            medals = list(range(1001,1701))
             ret = addToInventory(filepath, CS_Inv_Addr, medals, MEDAL_ITEM, 1)
         elif cheat == 2:
             item = [214]
             for _ in range(5):
-                ret = addToInventory(filepath, CS_Inv_Addr, item, USABLE_ITEM, 99)
+                ret = addToInventory(filepath, CS_Inv_Addr, item, USABLE_ITEM, 99, True)
         elif cheat == 3:
             items = range(202, 209)
             ret = addToInventory(filepath, CS_Inv_Addr, items, USABLE_ITEM, 99)
@@ -205,40 +236,19 @@ def main():
             items = [112, 115, 118, 121, 124, 127]
             ret = addToInventory(filepath, CS_Inv_Addr, items, USABLE_ITEM, 99)
         elif cheat == 5:
-            equipment = list(range(395, 401))
             ret = addToInventory(filepath, CS_Inv_Addr, equipment, EQUIP_ITEM, 99)
-        elif cheat == 6:
-            accessories = [10101, 12401, 10201, 12501, 10301, 10302, 12601, 
-                10401, 10501, 10502, 12701, 12702, 12801, 
-                12901, 13001, 10701, 10801, 10901, 10902, 
-                13101, 13102, 13201, 11001, 11101, 13301, 
-                13401, 11201, 11301, 13501, 11401, 11402, 
-                11403, 13601, 11501, 11601, 13701, 13801, 
-                13901, 11701, 11702, 11703, 14001, 14101, 
-                11801, 11802, 14201, 11901, 12001, 12002, 
-                12003, 14301, 12101, 12102, 12103, 14401, 
-                14402, 14501, 12201, 12301, 12302, 12303]  
+        elif cheat == 6: 
             ret = addToInventory(filepath, CS_Inv_Addr, accessories, ACCESSORY_ITEM, 50)
         elif cheat == 7:
-            try: 
-                with open(filepath, 'rb+') as f: 
-                    f.seek(0x9CC)
-                    for _ in range(351):
-                        f.read(4)
-                        f.write(int(3).to_bytes(4, 'little'))
-                ret = 0
-            except:
-                ret = 1
+            offset = 0x9CC
+            for _ in range(351):
+                offset += 4
+                ret = write32(filepath, offset, 3)
         elif cheat == 8:
-            try: 
-                with open(filepath, 'rb+') as f: 
-                    f.seek(CS_DigiConvert_Addr)
-                    for _ in range(346):
-                        f.read(2)
-                        f.write(int(200).to_bytes(2, 'little'))
-                ret = 0
-            except:
-                ret = 1
+            offset = CS_DigiConvert_Addr
+            for _ in range(346):
+                offset += 2
+                write16(filepath, offset, 200)
         elif cheat == 9:
             ret = allItems(filepath, CS_Inv_Addr)
         elif cheat == 10:
@@ -255,12 +265,11 @@ def main():
     if game == HACKERS_MEMORY or game == (CYBER_SLEUTH + HACKERS_MEMORY):
         print('Executing cheat for Hacker\'s Memory...')
         if cheat == 1:
-            medals = list(range(1001,1701))
             ret = addToInventory(filepath, HM_Inv_Addr, medals, MEDAL_ITEM, 1)
         elif cheat == 2:
             item = [214]
             for _ in range(5):
-                ret = addToInventory(filepath, HM_Inv_Addr, item, USABLE_ITEM, 99)
+                ret = addToInventory(filepath, HM_Inv_Addr, item, USABLE_ITEM, 99, True)
         elif cheat == 3:
             items = list(range(202, 209))
             ret = addToInventory(filepath, HM_Inv_Addr, items, USABLE_ITEM, 99)
@@ -268,40 +277,19 @@ def main():
             items = [112, 115, 118, 121, 124, 127]
             ret = addToInventory(filepath, HM_Inv_Addr, items, USABLE_ITEM, 99)
         elif cheat == 5:
-            equipment = list(range(395, 401))
             ret = addToInventory(filepath, HM_Inv_Addr, equipment, EQUIP_ITEM, 99)
-        elif cheat == 6:
-            accessories = [10101, 12401, 10201, 12501, 10301, 10302, 12601, 
-                10401, 10501, 10502, 12701, 12702, 12801, 
-                12901, 13001, 10701, 10801, 10901, 10902, 
-                13101, 13102, 13201, 11001, 11101, 13301, 
-                13401, 11201, 11301, 13501, 11401, 11402, 
-                11403, 13601, 11501, 11601, 13701, 13801, 
-                13901, 11701, 11702, 11703, 14001, 14101, 
-                11801, 11802, 14201, 11901, 12001, 12002, 
-                12003, 14301, 12101, 12102, 12103, 14401, 
-                14402, 14501, 12201, 12301, 12302, 12303]  
+        elif cheat == 6: 
             ret = addToInventory(filepath, HM_Inv_Addr, accessories, ACCESSORY_ITEM, 50)
         elif cheat == 7:
-            try: 
-                with open(filepath, 'rb+') as f: 
-                    f.seek(0x9CC)
-                    for _ in range(351):
-                        f.read(4)
-                        f.write(int(3).to_bytes(4, 'little'))
-                ret = 0
-            except:
-                ret = 1
+            offset = 0x9CC
+            for _ in range(351):
+                offset += 4
+                ret = write32(filepath, offset, 3)
         elif cheat == 8:
-            try: 
-                with open(filepath, 'rb+') as f: 
-                    f.seek(HM_DigiConvert_Addr)
-                    for _ in range(346):
-                        f.read(2)
-                        f.write(int(200).to_bytes(2, 'little'))
-                ret = 0
-            except:
-                ret = 1
+            offset = HM_DigiConvert_Addr
+            for _ in range(346):
+                offset += 2
+                write16(filepath, offset, 200)
         elif cheat == 9:
             ret = allItems(filepath, HM_Inv_Addr)
         elif cheat == 10:
@@ -328,6 +316,7 @@ def main():
         try:
             copy2(filepath+'.bak', filepath)
             print('Backup restored successfully.\n')
+            remove(filepath+'.bak')
         except:
             print('Could not restore backup file. Please manually rename backup file.\n')
 
